d988d7329c73efb6705ad38e637c5e26c9b88d2f
L-ucasOliveira <lucaza5008@gmail.com>
2023-07-10 19:21:59 -0300

    Formulário de cadastro de sala corrigido

--- Telas/formCadSala.php
+++ Telas/formCadSala.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário para remoção de Sala</title>
+    <title>Formulário de Cadastro de Sala</title>
 </head>
 <body>
-    <form action="removerSala.php" method="get">
+    <form action="cadastrarSala.php" method="get">
         <p>
-            Número da Sala: <input type="number" id="numSala" name="numSala">
+            Número: <input type="number" id="numSala" name="numSala">
+        </p>
+        <p>
+            Lotação:  <input type="number" id="lotacao" name="lotacao">
         </p>
         <input type="submit">
     </form>
